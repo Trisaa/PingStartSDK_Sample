574635c5dcb737b0b5dd65c4d55d4a2415dae4dd
Terry <1141775704@qq.com>
2015-12-22 09:20:25 +0800

    Delete: 删掉部分没用到资源，注册崩溃日志抓取

--- app/src/main/java/com/pingstart/PingStartApplication.java
+++ app/src/main/java/com/pingstart/PingStartApplication.java
@@ -6,9 +6,9 @@ import com.pingstart.utils.MyCrashHandler;
 
 public class PingStartApplication extends Application {
 
-	@Override
-	public void onCreate() {
-		MyCrashHandler.getInstance().register(this);
+    @Override
+    public void onCreate() {
+        MyCrashHandler.getInstance().register(this);
 
-	}
+    }
 }
